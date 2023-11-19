33ea285f6fae9103e4817181af8287300bf2a89a
Shawn Rodgers <me@directedbyshawn.com>
2023-11-19 01:34:07 -0500

    reorganize directory

--- public/client.js
+++ public/client.js
@@ -2,6 +2,12 @@ import { io } from "socket.io-client"
 
 const button = document.getElementById("test-button")
 
+const socket = io("http://localhost:3000")
+
+socket.on("connect", () => {
+    console.log("connected with id: ${socket.id}")
+})
+
 button.addEventListener("click", () => {
     console.log("yerrrrr")
 })
