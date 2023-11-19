6665daf05f33757fbfd4c1077745445bc0a80fcd
Shawn Rodgers <me@directedbyshawn.com>
2023-11-19 02:07:24 -0500

    socket connection made

--- public/client.js
+++ public/client.js
@@ -1,8 +1,6 @@
-import { io } from "socket.io-client"
-
 const button = document.getElementById("test-button")
 
-const socket = io("http://localhost:3000")
+const socket = io("http://localhost:5000")
 
 socket.on("connect", () => {
     console.log("connected with id: ${socket.id}")
--- src/server.js
+++ src/server.js
@@ -1,4 +1,16 @@
-const io = require("socket.io")(3000)
+const express = require("express")
+const { createServer } = require("http")
+const { Server } = require("socket.io")
+
+const app = express()
+const httpServer = createServer(app)
+const io = new Server(httpServer)
+
+app.use(express.static("public"))
+
+const PORT = 5000
+httpServer.listen(PORT)
+io.listen(3000)
 
 io.on("connection", socket => {
     console.log(socket.id)
