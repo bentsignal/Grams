22e5653a9abe4e11ae56828cb21cd0edd9b0e86a
Shawn Rodgers <me@directedbyshawn.com>
2023-11-19 02:28:04 -0500

    connection established, unable to emit custom events

--- public/client.js
+++ public/client.js
@@ -3,9 +3,16 @@ const button = document.getElementById("test-button")
 const socket = io("http://localhost:5000")
 
 socket.on("connect", () => {
-    console.log("connected with id: ${socket.id}")
+    console.log(`connected with id: ${socket.id}`)
 })
 
 button.addEventListener("click", () => {
-    console.log("yerrrrr")
+    console.log("Button has been pressed, sending message to server")
+    socket.emit("press", {
+        message: "button has been pressed"
+    })
+})
+
+socket.on("press_received", (message) => {
+    console.log(`New message from server: ${message}`)
 })
--- src/server.js
+++ src/server.js
@@ -9,9 +9,16 @@ const io = new Server(httpServer)
 app.use(express.static("public"))
 
 const PORT = 5000
+
 httpServer.listen(PORT)
-io.listen(3000)
 
 io.on("connection", socket => {
     console.log(socket.id)
+})
+
+io.on("press", (message) => {
+    console.log(`New message from client: ${message}`)
+    socket.emit("press_received", {
+        message: "This is the server, your button press was received"
+    })
 })
