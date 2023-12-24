86babcb3a6fe4b66ac9a50a3de2708bde3d4e213
Shawn Rodgers <me@directedbyshawn.com>
2023-12-24 03:28:44 -0500

    finished styling keybinds page

--- public/binds.js
+++ public/binds.js
@@ -10,17 +10,27 @@ class Binds {
             title: "Keybinds",
             content: `
                 <div id="keybinds-popup-wrapper">
-                    <div>Press a key to change it</div>
+                    <div id="keybind-instructions">Press a key to change it</div>
                     <div id="keybinds-popup-container">
                         <div class="keybind" id="mute-keybind">
+                            <p class="bind wrapper">${this.toggleMuteAll}</p>
+                            <p>Toggle mute all sounds</p>
                         </div>
                         <div class="keybind" id="shuffle-keybind">
+                            <p class="bind wrapper">${this.shuffle}</p>
+                            <p>Shuffle letters</p>
                         </div>
                         <div class="keybind" id="clear-keybind">
+                            <p class="bind wrapper">Space</p>
+                            <p>Clear letters</p>
                         </div>
                         <div class="keybind" id="emote-keybind">
+                           <p class="bind wrapper">${this.emote}</p>
+                           <p>Open emote menu</p>
                         </div>
                         <div class="keybind" id="chat-keybind">
+                            <p class="bind wrapper">${this.chat}</p>
+                            <p>Quickchat</p>
                         </div>
                     </div>
                 </div>
@@ -29,8 +39,16 @@ class Binds {
             titleColor: "white",
             textColor: "white",
             closeColor: "white",
+            css: `
+                .popup-body {
+                    margin: 0 !important
+                }
+            `,
+            loadCallback: () => {
+                const wrapper = document.getElementById("keybinds-popup-wrapper")
+                wrapper.innerHTML = wrapper.innerHTML.replaceAll("<p></p>", "")
+            }
         })
-
     }
 }
 
